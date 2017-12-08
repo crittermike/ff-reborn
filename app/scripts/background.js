6d3f196f66cf6d9fb2555549d476d741718c81d4
Mike Crittenden <mikecrittenden@gmail.com>
2017-12-08 15:57:16 -0500

    Make the shortcut focus the correct window if inactive

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,9 +3,11 @@
 
 chrome.commands.onCommand.addListener(function (command) {
   if (command === 'ff-show-tab') {
-    chrome.tabs.query({url: 'https://*.force.com/*', currentWindow: true}, (tabs) => {
+    chrome.tabs.query({url: 'https://*.force.com/*'}, (tabs) => {
       let tabId = tabs[0].id
+      let windowId = tabs[0].windowId
       chrome.tabs.update(tabId, {active: true})
+      chrome.windows.update(windowId, {focused: true})
     })
   }
 })
